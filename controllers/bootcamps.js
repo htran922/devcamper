a1504b17b0347e1e4ccd80b1cedda85a48d24ed1
Helen <h.tran922@gmail.com>
2019-10-24 19:27:12 -0400

    Mongoose duplicate key & validation error handling, async/await middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,90 +1,69 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
+
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
-
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-    } catch (err) {
-        next(err);
-    }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-        // check for if bootcamp id is correctly formatted but doesn't exist
-        if(!bootcamp) {
-            // need return statement because we have two responses within the try block
-            // if we don't have the return we will get error saying header is already sent
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (err) {
-        // checks for not correctly formated id
-        next(err);
+    // check for if bootcamp id is correctly formatted but doesn't exist
+    if(!bootcamp) {
+        // need return statement because we have two responses within the try block
+        // if we don't have the return we will get error saying header is already sent
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
-};
+    res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Create new bootcamps
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
-
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (err) {
-        next(err);
-    }
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
     
-};
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+
+});
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+   
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (err) {
-        next(err);
+    if(!bootcamp){
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
-    
-};
+    res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-        res.status(200).json({ success: true, data: {} });
-
-    } catch (err) {
-        next(err);
+    if(!bootcamp){
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
-};
+    res.status(200).json({ success: true, data: {} });
+});
