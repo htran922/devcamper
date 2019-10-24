eaa62b9211855e67ec5cd6288654a6cc5ecf9949
Helen <h.tran922@gmail.com>
2019-10-23 23:31:05 -0400

    Implemented bootcamp CRUD functionality

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
-dotenv.config({ path: './config/config.env' })
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 // Only want to run if we are in development environment
@@ -21,4 +30,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow.bold));
+
+// Handle unhandled promise rejections
+// We get an error message for what happens and app crashes, instead of app working
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
