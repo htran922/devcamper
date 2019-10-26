e5f9936a6bf15a81ad34dca081ce9c1be295d5b6
Helen <h.tran922@gmail.com>
2019-10-25 21:33:03 -0400

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,8 +8,75 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+    let query;
+
+    // Copy req.query
+    const reqQuery = { ...req.query };
+
+    // Fields to exclude that we don't want to be matched when filtering
+    const removeFields = ['select', 'sort', 'page', 'limit'];
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    // Create operators($gt, $gte, etc.)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    // Finding resource 
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    // Select Fields
+    if(req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sort
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        // Default sort by date
+        query = query.sort('-createdAt');
+    }
+
+    // Pagination
+    // Page 1 is default unless otherwise specified
+    const page = parseInt(req.query.page, 10) || 1;
+    // 100 per page is default
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit; 
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+
+
+    query = query.skip(startIndex).limit(limit);
+
+    // Executing query
+    const bootcamps = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    // If there is no previous page or if there is no next page, we don't want to show
+    if(endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit                   // same as doing limit:limit
+        }
+    }
+
+    if(startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+    res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
+
 });
 
 // @desc    Get single bootcamp
