a2a0f73e2cc63775fceb5e8e2d3c2ef628963626
Helen <h.tran922@gmail.com>
2019-11-05 13:18:33 -0500

    Forgot password & reset password functionality

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -38,12 +39,22 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt passwor using bcrypt
 UserSchema.pre('save', async function(next) {
+
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    // This down here will only run if the password is modified
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function() {
+    if(this.isModified()) {
+
+    }
+
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
@@ -53,5 +64,19 @@ UserSchema.methods.getSignedJwtToken = function() {
 UserSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
 
 module.exports = mongoose.model('User', UserSchema);
